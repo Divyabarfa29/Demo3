47cd70c9adc79ffa9b3ffcca8b55ddb444aaa651
divya <divyabarfa29@gmail.com>
2024-02-28 00:51:29 +0530

    one

---
+++ Demo1.java
@@ -0,0 +1,15 @@
+class Demo1  
+{
+  public static void main(String ar[])
+  {
+  if(1)
+  {
+	  System.out.println("Softwaves_1);
+  }
+  elseif(0)
+	  System.out.println("softwaves_2");
+  else
+	  System.out.println("softwaves_3");
+  }
+
+}
